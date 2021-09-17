065a276d874a6d7b9158019da89c60bbd20651c3
Vu.Tran <vu.tran@alta.com.vn>
2021-09-18 00:18:28 +0700

    Add quest layout

---
+++ Assets/DisableScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableScript : MonoBehaviour
+{
+    public GameObject objectToDisable;
+    public void _Disable()
+    {
+        if (objectToDisable != null)
+            objectToDisable.SetActive(false);
+        else
+            gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+    public Animator animator;
+
+    public Sprite disable;
+    public Sprite enable;
+
+    private Image image;
+    private bool status;
+
+    private void Start()
+    {
+        image = gameObject.GetComponent<Image>();
+        status = false;
+    }
+
+    public void ChangeSprite()
+    {
+        status = !status;
+
+        if (status)
+            image.sprite = enable;
+        else
+            image.sprite = disable;
+    }
+
+    public void PlayAnim(string animName)
+    {
+        animator.SetTrigger(animName);
+    }
+}
