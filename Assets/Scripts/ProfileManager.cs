8bdcd942d1ae617e4d3a9a2a11298eec5ac81e8f
Vu.Tran <vu.tran@alta.com.vn>
2021-09-18 14:55:50 +0700

    Add challenge layout

--- Assets/Scripts/ProfileManager.cs
+++ Assets/Scripts/ProfileManager.cs
@@ -46,68 +46,71 @@ public class ProfileManager : MonoBehaviour
         profileText.text = "*This is just a general description of your designer trait as a fictional character and should not be taken seriously. For a more qualitative evaluation, please visit and participate in the Weekly Challenge.*";
 
         // Race
-        profileText.text += string.Format(character.race.ProfileInfo, '\n');
-        //Debug.Log("race: " + character.race.Name);
-        //Debug.Log("trait: " + character.trait.Name);
+        profileText.text += ("\n" + string.Format(character.race.ProfileInfo, '\n'));
 
         // Trait
         if (character.race.Name.Equals("Werewolf"))
         {
             if (character.trait.Name.Equals("Rational"))
-                profileText.text += "\n\n- Werewolves are inherently emotional, even with rational characteristics. You tend to focus on the motive of other people, or the story of the design.";
+                profileText.text += "\n\nRational Werewolf:\n- Werewolves are inherently emotional, even with rational characteristics. You tend to focus on the motive of other people, or the story of the design.";
             else if (character.trait.Name.Equals("Creative"))
-                profileText.text += "\n\n- Werewolves are active. A creative Werewolf like you is bold. To take your idea further, you are not afraid of repeating the design process.";
+                profileText.text += "\n\nCreative Werewolf:\n- Werewolves are active. A creative Werewolf like you is bold. To take your idea further, you are not afraid of repeating the design process.";
             else if (character.trait.Name.Equals("Emotional"))
-                profileText.text += "\n\n- You pay attention to other people’s feelings and show constant concern of ethnicity in your design. Some emotional Werewolves easily show strong compassion for fictional characters.";
+                profileText.text += "\n\nEmotional Werewolf:\n- You pay attention to other people’s feelings and show constant concern of ethnicity in your design. Some emotional Werewolves easily show strong compassion for fictional characters.";
         }
         else if (character.race.Name.Equals("Angel"))
         {
             if (character.trait.Name.Equals("Rational"))
-                profileText.text += "\n\n- Rational Angels are sometimes assertive. Because you are used to reading the environment, you know how to control the atmosphere.";
+                profileText.text += "\n\nRational Angel:\n- Rational Angels are sometimes assertive. Because you are used to reading the environment, you know how to control the atmosphere.";
             else if (character.trait.Name.Equals("Creative"))
-                profileText.text += "\n\n- The status of Angels rely on their companions. You are naturally playful and your boldness is multiplied when you are with your friends.";
+                profileText.text += "\n\nCreative Angel:\n- The status of Angels rely on their companions. You are naturally playful and your boldness is multiplied when you are with your friends.";
             else if (character.trait.Name.Equals("Emotional"))
-                profileText.text += "\n\n- You usually prioritize the tasks based on your interest. The results are usually so expressive that other people can tell if you like it or not during the design process.";
+                profileText.text += "\n\nEmotional Angel:\n- You usually prioritize the tasks based on your interest. The results are usually so expressive that other people can tell if you like it or not during the design process.";
         }
         else if (character.race.Name.Equals("Wisp"))
         {
             if (character.trait.Name.Equals("Rational"))
-                profileText.text += "\n\n- Wisps tend to please others rather than themself, therefore, they are often worn out. However, a rational Wisp like you can distinguish the border of responsibility and avoid being led by others.";
+                profileText.text += "\n\nRational Wisp:\n- Wisps tend to please others rather than themself, therefore, they are often worn out. However, a rational Wisp like you can distinguish the border of responsibility and avoid being led by others.";
             else if (character.trait.Name.Equals("Creative"))
-                profileText.text += "\n\n- You are more active than other Wisps. Not only follow the instruction, you also find ways to improve it.";
+                profileText.text += "\n\nCreative Wisp:\n- You are more active than other Wisps. Not only follow the instruction, you also find ways to improve it.";
             else if (character.trait.Name.Equals("Emotional"))
-                profileText.text += "\n\n- You are innocent and passionate. Your enthusiasm when supporting others puts a smile on their faces.";
+                profileText.text += "\n\nEmotional Wisp:\n- You are innocent and passionate. Your enthusiasm when supporting others puts a smile on their faces.";
         }
         else if (character.race.Name.Equals("Elf"))
         {
             if (character.trait.Name.Equals("Rational"))
-                profileText.text += "\n\n- When developing new ideas, you focus on their functionality and practical aspects. You tend to set a goal for each task to make sure each step you took was executed well.";
+                profileText.text += "\n\nRational Elf:\n- When developing new ideas, you focus on their functionality and practical aspects. You tend to set a goal for each task to make sure each step you took was executed well.";
             else if (character.trait.Name.Equals("Creative"))
-                profileText.text += "\n\n- Elf are naturally playful. A creative Elf like you is usually a workaholic. You have a list of things you want to do and it never stops expanding.";
+                profileText.text += "\n\nCreative Elf:\n- Elf are naturally playful. A creative Elf like you is usually a workaholic. You have a list of things you want to do and it never stops expanding.";
             else if (character.trait.Name.Equals("Emotional"))
-                profileText.text += "\n\n- You easily feel passionate. You constantly inspire others through your design. Sometimes you put too much emotion in negative feedback that it affects your motivation.";
+                profileText.text += "\n\nEmotional Elf:\n- You easily feel passionate. You constantly inspire others through your design. Sometimes you put too much emotion in negative feedback that it affects your motivation.";
         }
         else if (character.race.Name.Equals("Human"))
         {
             if (character.trait.Name.Equals("Rational"))
-                profileText.text += "\n\n- Humans are usually not self motivated. Rational Humans like you usually work better under pressure.";
+                profileText.text += "\n\nRational Human:\n- Humans are usually not self motivated. Rational Humans like you usually work better under pressure.";
             else if (character.trait.Name.Equals("Creative"))
-                profileText.text += "\n\n- Humans have wide connections. You can see potential in other people and want to improve with them.";
+                profileText.text += "\n\nCreative Human:\n- Humans have wide connections. You can see potential in other people and want to improve with them.";
             else if (character.trait.Name.Equals("Emotional"))
-                profileText.text += "\n\n- You usually keep the ideas to yourself because you don’t want to take other people's chances to express themself. Keep in mind that sometimes you could be selfish because the people you care about also care about you.";
+                profileText.text += "\n\nEmotional Human:\n- You usually keep the ideas to yourself because you don’t want to take other people's chances to express themself. Keep in mind that sometimes you could be selfish because the people you care about also care about you.";
         }
         else if (character.race.Name.Equals("Mermaid"))
         {
             if (character.trait.Name.Equals("Rational"))
-                profileText.text += "\n\n- Mermaids are born rational, which makes rational Mermaids perfectionists. You value the learning outcomes and foresee how to achieve it.";
+                profileText.text += "\n\nRational Mermaid:\n- Mermaids are born rational, which makes rational Mermaids perfectionists. You value the learning outcomes and foresee how to achieve it.";
             else if (character.trait.Name.Equals("Creative"))
-                profileText.text += "\n\n- You are bold. You are not stressed out about the design outcome because you are confident and only want to express yourself through design.";
+                profileText.text += "\n\nCreative Mermaid:\n- You are bold. You are not stressed out about the design outcome because you are confident and only want to express yourself through design.";
             else if (character.trait.Name.Equals("Emotional"))
-                profileText.text += "\n\n- You are expressive and supportive. You like to have a competitive companion to constantly improve each other.";
+                profileText.text += "\n\nEmotional Mermaid:\n- You are expressive and supportive. You like to have a competitive companion to constantly improve each other.";
         }
 
+        profileText.text += ("\n\n" + character.cloth.Name + ":");
+        profileText.text += string.Format(character.cloth.ProfileInfo, '\n');
+        profileText.text += ("\n\n" + character.hobby.Name + ":");
         profileText.text += string.Format(character.hobby.ProfileInfo, '\n');
+        profileText.text += ("\n\n" + character.table.Name + " table :");
         profileText.text += string.Format(character.table.ProfileInfo, '\n');
+        profileText.text += ("\n\n" + character.wall.Name + " wall :");
         profileText.text += string.Format(character.wall.ProfileInfo, '\n');
     }
 
